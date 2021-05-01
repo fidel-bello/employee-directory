a413362daa9abf75ce6914aead229dbaba21d806
fidel bello <59423@bears.smcc.edu>
2021-05-01 18:08:17 -0400

    added function navbar to be the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Footer />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Navbar() {
+    return (
+    <nav>
+        <h1>Employee Directory</h1>
+    </nav>
+    )
+}
+
+export default Navbar;
