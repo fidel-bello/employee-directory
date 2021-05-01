b90d4490d8ffe97efee4c8df978ec77bac833c97
fidel bello <59423@bears.smcc.edu>
2021-05-01 17:54:29 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+        <footer>
+        <ul className="footer">
+            <li>Random</li>
+        </ul>
+        </footer>
+    )
+}
+
+export default Footer;
