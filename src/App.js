45c5acaa187833b9d2d16dc73999deaa99000512
fidel bello <59423@bears.smcc.edu>
2021-05-01 20:24:19 -0400

    setting up main

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import Wrapper from './components/Wrapper'
+
+function App () {
+  return (
+    <div className="App">
+      <Wrapper>
+
+      </Wrapper>
+    </div>
+  )
+}
+
+export default App;
