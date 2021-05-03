0c0537938463928b5c0d49da5e01258857de5a3e
fidel bello <59423@bears.smcc.edu>
2021-05-03 17:24:32 -0400

    made card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Wrapper from './components/Wrapper'
+import Card from "./components/Cards"
+
 
 function App () {
   return (
     <div className="App">
       <Wrapper>
+        <Card />
 
       </Wrapper>
     </div>
