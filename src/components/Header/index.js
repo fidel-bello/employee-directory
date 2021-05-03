904d83693767790e09b995f0ea299baf1ab09cf4
fidel bello <59423@bears.smcc.edu>
2021-05-03 18:20:06 -0400

    added header as employee directory

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+
+function Header() {
+    return (
+        <div className="wrapper">
+            <div className="header">
+        Employee Directory
+            </div>
+        </div>
+    )
+}
+export default Header;
