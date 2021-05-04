84e766ab030c33c250f9dc55517261bf3bd665d9
fidel bello <59423@bears.smcc.edu>
2021-05-03 22:23:21 -0400

    made body that has alphabitazion options for sorting and also a search bar that, eventually will be functional

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './index.css'
 
 function Body(){ 
     return (
-        <div className="bodyContainer">
-            <label>Search Employee Name</label>
-            <input type='text' placeholder='enter name here' />
+        <div className="formContainer">
+        <div className="centerMe largerText">
+            <input className="radio" type="radio" value="firstName" name="name" /> Alphabetize
+            <input className="radio" type="radio" value="lastName" name="name" />   Unalphabetize
         </div>
+        <form>
+            <div className="centerMe largerText">
+                <p className="inline">Search Employee:</p>
+                <input type="text" id="employeeName" className="input" placeholder="name here"/>
+            </div>
+            <div className="custom-flex flex-parent">
+                <input className="btn submitBtn" type="submit" />
+                <button className="btn dangerBtn">Clear Filter</button>
+            </div>
+        </form>
+    </div>
     )
 }
 export default Body;
