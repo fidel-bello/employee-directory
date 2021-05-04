e6baf1aaacf8fb37ecd4d48061a9ddd997afffb0
fidel bello <59423@bears.smcc.edu>
2021-05-04 00:03:17 -0400

    added btn and search bar

---
+++ src/components/Btn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Btn(props) {
+    return (
+        <div className='col-3 d-inline'>
+            <div className="btn btn-success"></div>
+        </div>
+    )
+}
+export default Btn;
