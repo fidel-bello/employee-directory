bee52e0b566eaaf88aef9db6975f42a5bd18a7f6
fidel bello <59423@bears.smcc.edu>
2021-05-03 21:34:16 -0400

    added employee search bar

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-function Body {
-    
+function Body(){ 
+    return (
+        <div className="bodyContainer">
+            <label>Search Employee Name</label>
+            <input type='text' placeholder='enter name here' />
+        </div>
+    )
 }
+export default Body;
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card';
+import Body from '../Body';
 
 
 
@@ -8,6 +9,7 @@ function Container() {
     //returns components
     return (
         <div>
+        <Body />
         <Card />
         </div>
     );
